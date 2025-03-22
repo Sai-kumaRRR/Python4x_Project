cd29bf72ae8765c3d8d4d8f934040e68529525c9
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-03-22 20:05:36 +0530

    22th March

---
+++ March/ex07_functions/Lab075_functions_examples.py
@@ -0,0 +1,13 @@
+def home():
+    private_toilet = "PT"
+    print(public_toilet)
+    print(private_toilet)
+
+    def strange():
+        print(public_toilet)
+        # print(private_toilet)
+
+        print(public_toilet)
+        # print(private_toilet)
+
+        # local variables have more preference as compare to public variables
