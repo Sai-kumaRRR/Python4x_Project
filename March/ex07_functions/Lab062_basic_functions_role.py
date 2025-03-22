b16a045d008edd0c86975847b55d547141f4228c
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-03-22 09:24:24 +0530

    22th March

---
+++ March/ex07_functions/Lab062_basic_functions_role.py
@@ -0,0 +1,15 @@
+# Basic functions
+
+# part01- define
+
+def greet():
+    print("Hello , welcome to python World!")
+
+    # part 02 - call
+    greet()
+    greet()
+    greet()
+
+    print("dasasda")
+
+    greet()
---
+++ March/ex07_functions/Lab063_basic_part02.py
@@ -0,0 +1,20 @@
+# Basic function
+
+
+# part 01 - Define
+
+def greet():
+    print("Hello,Welcome to python World!")
+    print("2")
+    print("4")
+
+    # Part 02 - call
+    greet()
+    greet()
+    greet()
+
+    print("dasdasadas")
+    greet()
+
+
+
