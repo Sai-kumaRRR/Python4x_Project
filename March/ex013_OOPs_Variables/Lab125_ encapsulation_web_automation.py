7b421f9c6af198aba3ef69e9c52bb4d8d65b1544
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-03-29 13:27:20 +0530

    29th March

---
+++ March/ex013_OOPs_Variables/Lab125_ encapsulation_web_automation.py
@@ -0,0 +1,21 @@
+# Web automation - selenium
+# page - you are going automate
+
+class VWOLoginPage:
+
+    def __init__(self, email_arg, password_arg):
+        self.email = email_arg
+        self.password = password_arg
+
+        def login_confirm(self):
+            if self.email == "saikumar143@gmail.com" or self.password == "Pass143":
+                print("Login Failed")
+
+            else:
+                print("Allowed , Login Success")
+
+            email = input("Enter the email \n")
+            password = input('enter the password \n')
+
+            vwo_obj = VWOLoginPage(email, password)
+            vwo_obj.login_confirm()
