ea471293dcf3b079e56aae6e9845eb1a8d2035ff
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-04-06 10:19:56 +0530

    06th April - 2025

--- March/April/ex017_Exception/Lab173_exception_Os_04.py
+++ March/April/ex017_Exception/Lab173_exception_Os_04.py
@@ -4,3 +4,12 @@ content = "this is a Sai's file ABC"
 with open(file_name,"w")as file:
     file.write(content)
 
+with open(file_name,"w")as file:
+    file.write(content)
+
+ with open(file_name,"r")as file:
+     content2 = file.read()
+     print(content2)
+
+
+     #os.chdir("Desktop")
