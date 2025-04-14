5da8f552f16c247d85a7f16feaf324df190cc96b
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-04-14 16:50:43 +0530

    14th April - 2025

--- March/April/ex017_Exception/test_Lab189_pytest_GET.py
+++ March/April/ex017_Exception/test_Lab189_pytest_GET.py
@@ -10,3 +10,13 @@ def test_get_request_positive():
     URL ="https://restful-booker.herokuapp.com/booking/1"
     response_data =requests.get(url=URL)
     assert response_data.status_code ==200
+
+
+@allure.title("verify the GET Request of restful booker with invalid ID")
+@allure.description("This Testcase check booking -1 and verify the response")
+@pytest.mark.positive
+def test_get_request_negative():
+    url_get="https://restful-booker.herokuapp.com/booking/-1"
+    response_data =requests.get(url=url_get)
+    assert response_data.status_code == 404
+
